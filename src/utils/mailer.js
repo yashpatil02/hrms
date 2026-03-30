28885ed52a5abf7bde1960c00e2b95d4e0e247d7
yashpatil02 <yashpatil121199@gmail.com>
2026-03-30 19:30:20 +0530

    fix milere nodemailer

--- src/utils/mailer.js
+++ src/utils/mailer.js
@@ -2,11 +2,15 @@ import nodemailer from "nodemailer";
 
 const createTransporter = () =>
   nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
     },
+    connectionTimeout: 10000,
+    socketTimeout: 20000,
   });
 
 /* ================================
